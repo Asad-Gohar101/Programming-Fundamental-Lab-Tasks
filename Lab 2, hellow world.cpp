cd7056fdb49ef25f658619dda5b8062d04d06c4d
Asad-Gohar101 <asadgohar001@gmail.com>
2025-10-19 12:06:53 -0700

    Add files via upload
    Lab 3 was not conducted due to the security reasons

---
+++ Lab 2, hellow world.cpp
@@ -0,0 +1,6 @@
+#include<cstdio> // This program is in C++ and I am using the header file <cstdio> (which is for C++) for input and output functions.
+int main() 
+{
+    printf("Hello, World!\n"); 
+    return 0; 
+}
